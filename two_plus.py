a8525a3304535fd8100557e3f3c20a3c453e7fac
DrYuan <35892064+DrYuan@users.noreply.github.com>
2019-03-28 00:29:35 +0800

    Add files via upload
    求首尾相接数组的最大子数组

---
+++ two_plus.py
@@ -0,0 +1,20 @@
+# A = input("请输入一组数字,用空格分隔")
+# L = list(map(int, A.split()))
+MAX = []
+def max_list(l):
+    max = l[0]
+    sum = 0
+    i = len(l)
+    for number in l:
+        sum += number
+        if sum > max:
+             max = sum
+        if sum < 0:
+             sum = 0
+    MAX.append(max)
+def Max(l):
+    for i in range(len(l)):
+        max_list(l)
+        l.append(l.pop(0))
+    return max(MAX)
+# print(Max(L))
